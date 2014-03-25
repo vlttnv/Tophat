b74b369c7e5f4e5440199620d16ae512d335ef83
Charles Wu <cjw.charleswu@gmail.com>
2014-03-25 18:59:51 +0000

    clean up

--- server/views.py
+++ server/views.py
@@ -1,28 +1,52 @@
-from flask import render_template, request, abort, jsonify
+from flask import render_template, request, abort, jsonify, g
 from server import app
+from datetime import datetime
 import os
+import sqlite3
 
 @app.route('/')
 @app.route('/index')
 def index():
     return 'Hello World'
 
-@app.route('/register', methods = ['POST'])
-def register():
-    if not request.json or not 'id' and 'location' in request.json:
-        abort(400)
+@app.route('/heartbeat', methods = ['POST'])
+def heartbeat():
+    if not request.json or not ('id' and 'location' in request.json):
+        return 'Heartbeat object not understood.\n', 400
         
     producer = {
+        'ip': request.remote_addr,
         'id': request.json['id'],
         'location': request.json['location']
     }
     
-    return jsonify( { 'producer': producer } ), 201
+    # updateDB(producer)
+
+    return 'Heartbeat recorded', 200
 
 @app.route('/get_location/<int:producer_id>', methods = ['GET'])
 def get_location(producer_id):
-    data = {
+
+    # getProducerLocation(producer_id)
+
+    location = {
         'location': 'St Andrews'
     }
 
-    return jsonify( { 'data': data } ), 201
+    return jsonify({ 'location': location }), 200
+
+@app.route('/send', methods = ['POST'])
+def receive():
+    if not request.json or not ('id' and 'location' and 'data' in request.json):
+        return 'Data object not understood.\n', 400
+
+    data = {
+        'id': request.json['id'],
+        'location': request.json['location'],
+        'data': request.json['data'],
+        'timestamp': datetime.utcnow()
+    }
+
+    # updateDB(data)
+
+    return 'Data recorded', 200
