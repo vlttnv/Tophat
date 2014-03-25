a93c7cde83331acbee6ef88b9a3924879a7a4aa8
Valentin Tunev <hdbuster@gmail.com>
2014-03-25 17:38:48 +0000

    Added first producer code

--- client/producer.py
+++ client/producer.py
@@ -1,1 +1,8 @@
-print "client"
+import requests, json
+from flask import jsonify
+
+payload = {'id': 'value', 'location': 'value2'}
+headers = {'content-type': 'application/json'}
+r = requests.post("http://138.251.207.92:5000/register", data=json.dumps(payload), headers=headers)
+print r.text
+print r.url
