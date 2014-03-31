42fdca6b40aa73027d1cd5137ef79674175ba636
Valentin Tunev <hdbuster@gmail.com>
2014-03-31 17:31:46 +0100

    Latest balancer

--- balancer/views.py
+++ balancer/views.py
@@ -1,6 +1,19 @@
 from balancer import app
 from flask import redirect
 
+workers = ['http://127.0.0.1:5000','http://127.0.0.1:5000','http://127.0.0.1:5000']
+
+ptr = 0
+
 @app.route('/get_data/<int:id>')
 def index(id):
-	return redirect("http://127.0.0.1:5000/get_data/" + str(id), code=302)
+	global ptr
+	url = workers[ptr] + "/get_data/" + str(id)
+	if ptr == len(workers) - 1:
+		ptr = 0
+	else:
+		ptr = ptr + 1
+
+	print hello
+	print len(workers)
+	return redirect(url, code=302)
