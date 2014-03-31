d29be1608462e3ea03faa312eec8d0abca2fc4b5
Charles Wu <cjw.charleswu@gmail.com>
2014-03-31 19:34:40 +0100

    added a simple test

--- tests/test_one.sh
+++ tests/test_one.sh
@@ -1,3 +1,18 @@
 #!/bin/bash
 
-echo 'Hello Test 1'
+printf '\n=========\n'
+printf 'Test Case One: 1 producer and 1 consumer.\n'
+
+printf 'Running producer: 1.\n'
+python ../client/producer.py $1 $2 1 &
+PID_PRODUCER=$!
+sleep 10
+
+printf 'Running consumer: 1.\n'
+python ../client/consumer.py $1 $2 1
+
+printf 'Killing producer: 1.\n'
+kill $PID_PRODUCER
+
+printf '\nTest Case One: Completed.\n'
+printf '=========\n\n'
