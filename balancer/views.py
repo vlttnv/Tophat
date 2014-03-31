55ce89e83795913a8925cc1fe929767769f9c1a6
Valentin Tunev <hdbuster@gmail.com>
2014-03-31 19:15:26 +0100

    Working on stuff

--- balancer/views.py
+++ balancer/views.py
@@ -1,8 +1,9 @@
 from balancer import app
-from flask import redirect
+from flask import redirect, request
 from server import db_manager
+import json
 
-workers = ['http://138.251.207.92:5001','http://138.251.207.92:5002','http://138.251.207.92:5003']
+workers = ['http://138.251.212.25:5000','http://138.251.212.25:5000','http://138.251.212.25:5000']
 
 ptr = 0
 
@@ -16,4 +17,28 @@ def index(id):
 		ptr = ptr + 1
 
 	print len(workers)
+	return redirect(url, code=302)
+
+@app.route('/heartbeat', methods=['POST'])
+def heartbeat():
+	"""
+
+	"""
+
+	print 'Request from', request.remote_addr, ': redirect to: '
+
+	if not request.json:
+		return 'Hearbeat object must be in json format.', 400
+
+	if not ('port' and 'id' and 'location' and 'data' in request.json):
+		return 'Heartbeat object not udnerstood.', 400
+
+	global ptr
+	headers = {'content-type': 'application/json'}
+	url = workers[ptr] + "/hearbeat"
+	if ptr == len(workers) - 1:
+		ptr = 0
+	else:
+		ptr = ptr + 1
+
 	return redirect(url, code=302)
